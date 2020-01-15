7df5d5452b9638f396bb9bf07cea46ab519bb36d
torkvell <tor.kvell@outlook.com>
2020-01-15 15:32:50 +0100

    removed node_modules from git

---
+++ index.js
@@ -0,0 +1,10 @@
+const board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+function isValidBoard(board) {
+    // ...implement your logic here, return true or false
+}
+module.exports = { isValidBoard }
